17c240c6e142c608903c0cd92fd946964939214c
Ahmet Hakan <hakan.kizilcubuk@gmail.com>
2024-04-30 23:05:01 +0300

    User Profile Pane is added

---
+++ src/main/java/org/example/projectjavafx/userProfileController.java
@@ -0,0 +1,73 @@
+package org.example.projectjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class userProfileController {
+
+    @FXML
+    private Button addFriendButton;
+
+    @FXML
+    private Rectangle addFriendRectangle;
+
+    @FXML
+    private Button changeProfilePicButton;
+
+    @FXML
+    private Rectangle changeProfilePictureRect;
+
+    @FXML
+    private ImageView coinImage;
+
+    @FXML
+    private Text coinNumber;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Rectangle deleteRect;
+
+    @FXML
+    private Text eMailText;
+
+    @FXML
+    private Text editProfileText;
+
+    @FXML
+    private Rectangle emailRect;
+
+    @FXML
+    private ScrollPane friendsScrollPane;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Rectangle logOutRect;
+
+    @FXML
+    private ImageView logoImage;
+
+    @FXML
+    private Text myAccountLabel;
+
+    @FXML
+    private Button savedButton;
+
+    @FXML
+    private Rectangle savedRect;
+
+    @FXML
+    private Circle userAvatar;
+
+    @FXML
+    private Text userNameText;
+
+}
