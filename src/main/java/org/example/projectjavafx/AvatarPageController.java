f44cc9ac102fa0ea690028836fafe3f113eae35e
Ahmet Hakan <hakan.kizilcubuk@gmail.com>
2024-04-30 16:08:09 +0300

    Login Pane and Avatar Page Pane is added

---
+++ src/main/java/org/example/projectjavafx/AvatarPageController.java
@@ -0,0 +1,43 @@
+package org.example.projectjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class AvatarPageController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private RadioButton avatar1;
+    private RadioButton avatar2;
+    private RadioButton avatar3;
+    private RadioButton avatar4;
+    private RadioButton avatar5;
+    private RadioButton avatar6;
+
+    ToggleGroup avatar;
+
+    @FXML
+    ImageView firstAvatar;
+    Image firstavatar = new Image(getClass().getResourceAsStream("/images/avatar (8).png"));
+
+
+    public void initialize()
+    {
+        avatar = new ToggleGroup();
+        avatar1.setToggleGroup(avatar);
+        avatar2.setToggleGroup(avatar);
+        avatar3.setToggleGroup(avatar);
+        avatar4.setToggleGroup(avatar);
+        avatar5.setToggleGroup(avatar);
+        avatar6.setToggleGroup(avatar);
+
+
+
+    }
+
+
+    }
---
+++ src/main/java/org/example/projectjavafx/LoginpageController.java
@@ -0,0 +1,34 @@
+package org.example.projectjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class LoginpageController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    ImageView logo;
+    Image myLogo = new Image(getClass().getResourceAsStream("/images/logo.png"));
+
+    @FXML
+    private TextField emailusernameTextField;
+    private TextField passwordTextFieldLogin;
+    private TextField confirmPasswordTextField;
+    private TextField nameTextField;
+    private TextField usernameTextField;
+    private TextField emailTextField;
+    private TextField passwordTextFieldSignup;
+
+    @FXML
+    private Button loginButton;
+    private Button signupButton;
+
+
+
+}
