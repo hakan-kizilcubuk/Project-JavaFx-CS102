c5a7b13c494f54d4ba93697aa5cf6d631a0bf5cd
Elif Duman <ElifDumand@users.noreply.github.com>
2024-05-01 10:17:27 +0300

    methods and variables added

--- src/main/java/org/example/projectjavafx/Question.java
+++ src/main/java/org/example/projectjavafx/Question.java
@@ -1,6 +1,35 @@
 package org.example.projectjavafx;
 
+import java.util.List;
+
 public class Question
 {
-    
+    protected String questionId;
+    protected String content;
+    protected User creator;
+    protected String mediaUrl;
+    protected Branch branch;
+    protected int difficultyLevel;
+    private List<String> options;
+    private String correctAnswer;
+    private boolean jokerCallAFriend;
+    private boolean jokerFiftyFifty;
+    private int time;
+    private int ratioOfCorrectAnswer;
+    private int answerCount;
+
+
+    public void updateContent(String content)
+    {
+        this.content = content;
+    }
+    public void attachMedia(String mediaLink)
+    public void difficultyMethod( int ratioOfCorrectAnswer)
+    public void calculateTimeLimit(int difficultyLevel)
+    public void answerQuestion(String userAnswer)
+    public void isCorrect(String userAnswer)
+    public void useJokerCallAFriend()
+    public void usejokerFiftyFifty()
+
+
 }
