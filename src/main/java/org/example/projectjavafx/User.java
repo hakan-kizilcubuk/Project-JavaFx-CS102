07f5f98eba276c2033008fef821bdd6fce7404f5
mertincesesli <mert.incesesli@ug.bilkent.edu.tr>
2024-05-01 10:41:41 +0300

    Branch,User and Ranking classes are implemented
    Branch,User and Ranking classes are implemented

---
+++ src/main/java/org/example/projectjavafx/User.java
@@ -0,0 +1,36 @@
+package org.example.projectjavafx;
+
+import java.util.ArrayList;
+
+public class User {
+    private int userId;
+    private String userName;
+    private ArrayList<Question> userQuestions;
+    private ArrayList<Question> savedQuestions;
+    private String userPassword;
+    private String userProfile;
+    private int userCoin;
+    private String userEmail;
+    private ArrayList<User> friends;
+    private int userRank;
+
+    public void changeProfilePicture(String picturePath){
+
+    }
+
+    public void addFriend(User user){
+
+    }
+
+    public void removeFriend(User user){
+
+    }
+
+    public void saveQuestion(Question question){
+
+    }
+
+    public ArrayList<User> getFriends() {
+
+    }
+}
