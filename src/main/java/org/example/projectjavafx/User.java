1abeb26a585aa955d97a6d7b914e70826d2ac999
Aral Müftüoğlu <aralmuftuoglu@hotmail.com>
2024-05-01 15:40:50 +0300

    user class updated

--- src/main/java/org/example/projectjavafx/User.java
+++ src/main/java/org/example/projectjavafx/User.java
@@ -14,20 +14,34 @@ public class User {
     private ArrayList<User> friends;
     private int userRank;
 
+
+    public User(String user_name,int id, String password,String email)
+    {
+        this.userName= user_name;
+        this.userId = id;
+        this.userPassword= password;
+        this.userEmail=email;
+        
+    }
+
     public void changeProfilePicture(String picturePath){
+
 
     }
 
     public void addFriend(User user){
 
+        friends.add(user);
     }
 
     public void removeFriend(User user){
 
+        friends.remove(user);
     }
 
     public void saveQuestion(Question question){
 
+        savedQuestions.add(question);
     }
 
     public ArrayList<User> getFriends() {
