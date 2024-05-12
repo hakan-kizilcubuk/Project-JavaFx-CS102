1d039455f7d14bfe5c64d0327fdfa5876db310e5
Ahmet Hakan <hakan.kizilcubuk@gmail.com>
2024-05-12 14:32:01 +0300

    Now we are able to add questions to database via add queston frame

--- src/main/java/org/example/projectjavafx/AddQuestionController.java
+++ src/main/java/org/example/projectjavafx/AddQuestionController.java
@@ -57,6 +57,9 @@ public class AddQuestionController implements Initializable {
     @FXML
     private Label usernameLabel;
 
+    @FXML
+    private Label questionAddedLabel;
+
     private String[] branch = {"History", "General Knowledge", "Geography", "Sports", "Science"};
 
     public void setUsernameLabel()
@@ -69,5 +72,46 @@ public class AddQuestionController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setUsernameLabel();
         branchChoiceBox.getItems().addAll(branch);
+    }
+
+    @FXML
+    public void addQuestionButtonOnAction(ActionEvent event) {
+        if (!questionTextField.getText().isBlank() && !choice1TextField.getText().isBlank() && !choice2TextField.getText().isBlank() && !choice3TextField.getText().isBlank() && !choice4TextField.getText().isBlank() && !branchChoiceBox.getValue().isEmpty()) {
+            addQuestionToBranch();
+        }
+
+        else{
+            questionAddedLabel.setText("Some parts are missing! Fill all fields");
+        }
+    }
+
+    @FXML
+    public void goBackButtonOnAction(ActionEvent event) {
+
+    }
+
+    public void addQuestionToBranch(){
+        DatabaseConnection connectDatabaseNow = new DatabaseConnection();
+        Connection connectDatabase = connectDatabaseNow.getConnection();
+
+        String question = questionTextField.getText();
+        String branch = branchChoiceBox.getValue();
+        String correctAnswer = choice1TextField.getText();
+        String wrongAnswer1 = choice2TextField.getText();
+        String wrongAnswer2 = choice3TextField.getText();
+        String wrongAnswer3 = choice4TextField.getText();
+        String authorOfQuestion = usernameLabel.getText();
+
+        String insertFields = "INSERT INTO question (username, wronganswer1, wronganswer2, wronganswer3, correctanswer, branch, question) VALUES ('";
+        String insertValues = authorOfQuestion + "','" + wrongAnswer1 + "','" + wrongAnswer2 + "','" + wrongAnswer3 + "','" + correctAnswer + "','" + branch + "','" + question + "')";
+        String insertToRegister = insertFields + insertValues;
+
+        try {
+            Statement statement = connectDatabase.createStatement();
+            statement.executeUpdate(insertToRegister);
+            questionAddedLabel.setText("Question Added to " + branchChoiceBox.getValue());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
