cda891080341c1374acd4f26b816e16ef0f07aeb
Elif Duman <ElifDumand@users.noreply.github.com>
2024-05-13 21:41:46 +0300

    Avatar Page is updated
    The radio buttons are bounded to the avatar images in the controller. In the fxml file the buttons are set to the toggle group avatar and the getProfilePicture method is added to each radio button for profile picture selection.

--- src/main/java/org/example/projectjavafx/AvatarPageController.java
+++ src/main/java/org/example/projectjavafx/AvatarPageController.java
@@ -1,6 +1,8 @@
 package org.example.projectjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -11,18 +13,10 @@ public class AvatarPageController {
     @FXML
     private Label welcomeText;
     @FXML
-    private RadioButton avatar1;
-    private RadioButton avatar2;
-    private RadioButton avatar3;
-    private RadioButton avatar4;
-    private RadioButton avatar5;
-    private RadioButton avatar6;
-
-    ToggleGroup avatar;
+    private Button goBackButton;
 
     @FXML
-    ImageView firstAvatar;
-    Image firstavatar = new Image(getClass().getResourceAsStream("/images/avatar (8).png"));
+    ToggleGroup avatar = new ToggleGroup();
 
 
     public void initialize() {
@@ -35,7 +29,49 @@ public class AvatarPageController {
         avatar6.setToggleGroup(avatar);
 
 
+
+    RadioButton avatar1 = createRadioButtonWithImage("avatar (8).png", avatar) ;
+    RadioButton avatar2 = createRadioButtonWithImage("avatar (7).png", avatar);
+    RadioButton avatar3 = createRadioButtonWithImage("avatar (6).png", avatar);
+    RadioButton avatar4 = createRadioButtonWithImage("avatar (5).png", avatar);
+    RadioButton avatar5 =  createRadioButtonWithImage("avatar (4).png", avatar);
+    RadioButton avatar6 = createRadioButtonWithImage("avatar (3).png", avatar);
+
+
+    @FXML
+    void goBackButtonOnAction(ActionEvent event) {
+        
+    }
+
+    //this method creates radiobuttons that are bounded to the images and sets their toggleGroups
+    public RadioButton createRadioButtonWithImage(String imagePath, ToggleGroup avatar)
+    {
+        ImageView imageView = new ImageView(new Image(imagePath));
+        imageView.setFitWidth(80);
+        imageView.setFitHeight(80);
+
+        //bounding the avatar images to the radio buttons
+        RadioButton button = new RadioButton();
+        button.setToggleGroup(avatar);
+        button.setGraphic(imageView);
+        
+        return button;
     }
 
 
 }
+
+    //this method will handle changing the profile picture when selected
+    @FXML
+    void getProfilePicture(ActionEvent event)
+    {
+        if(avatar1.isSelected()) {}
+        else if (avatar2.isSelected()) {}
+        else if (avatar3.isSelected()) {}
+        else if (avatar4.isSelected()) {}
+        else if (avatar5.isSelected()) {}
+        else if (avatar6.isSelected()) {}
+    }
+    
+    }
+
